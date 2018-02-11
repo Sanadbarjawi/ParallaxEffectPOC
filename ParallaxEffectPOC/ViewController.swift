111cf913f1f5f6160da6bcbd1dca84f6548df784
Sanad Barjawi <Sanadbarj@gmail.com>
2018-02-11 19:28:44 +0200

    Initial Commit

---
+++ ParallaxEffectPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxEffectPOC
+//
+//  Created by Sanad  on 2/11/18.
+//  Copyright © 2018 Sanad . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
